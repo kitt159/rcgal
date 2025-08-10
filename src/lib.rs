87c0d4ba967b3c28d1b242388491e4ff14d6a29f
kitt159 <ondra007.tom@seznam.cz>
2025-08-10 16:36:00 +0200

    Add 2D Point

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,21 @@
+#![warn(missing_docs)]
+
+//! # RCGAL
+
+mod point;
+
+pub use point::Point;
+
+#[doc(hidden)]
 #[cfg(test)]
-mod tests {
-    use super::*;
+#[macro_export]
+macro_rules! rel_eq {
+    ($left:expr, $right:expr) => {
+        ::approx::assert_relative_eq!(
+            $left,
+            $right,
+            epsilon = f64::MIN_POSITIVE,
+            max_relative = f64::EPSILON
+        );
+    };
 }
