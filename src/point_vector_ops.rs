15298f695524f2a906473c00607e6634df05104d
kitt159 <ondra007.tom@seznam.cz>
2025-08-15 13:05:33 +0200

    Implement Add 2D Vector for 2D Point

--- src/point_vector_ops.rs
+++ src/point_vector_ops.rs
@@ -1,11 +1,19 @@
 use crate::{Point, Vector};
-use std::ops::Sub;
+use std::ops::{Add, Sub};
 
 impl Sub for Point {
     type Output = Vector;
 
     fn sub(self, rhs: Self) -> Self::Output {
         Vector::new(self.x() - rhs.x(), self.y() - rhs.y())
+    }
+}
+
+impl Add<Vector> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Vector) -> Self::Output {
+        Point::new(self.x() + rhs.x(), self.y() + rhs.y())
     }
 }
 
@@ -24,5 +32,18 @@ mod tests {
         let c = a - b;
         rel_eq!(c.x(), -2.0);
         rel_eq!(c.y(), -2.0);
+    }
+
+    #[test]
+    fn point_2d_add_vector_2d() {
+        let a = Point::new(1.0, 2.0);
+        let b = Vector::new(-1.0, -2.0);
+        let c = a + b;
+        rel_eq!(c.x(), 0.0);
+        rel_eq!(c.y(), 0.0);
+        let b = Vector::new(3.0, 4.0);
+        let c = a + b;
+        rel_eq!(c.x(), 4.0);
+        rel_eq!(c.y(), 6.0);
     }
 }
